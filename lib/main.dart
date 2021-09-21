6f57c31d673ac4754f6588b91e3bad2e15830950
TixSpik <luisrhxq@hotmail.com>
2021-09-21 17:51:38 -0500

    removing widgets from main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_exercise_2/screen/home_page.dart';
 import 'package:get/get.dart';
-import 'package:flutter_exercise_2/screen/widgets/custom_container.dart';
-import 'package:flutter_exercise_2/screen/widgets/design1_widget.dart';
-import 'package:flutter_exercise_2/screen/widgets/design2_widget.dart';
-import 'package:flutter_exercise_2/screen/widgets/design3_widget.dart';
-import 'package:flutter_exercise_2/screen/widgets/design4_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -41,53 +36,5 @@ class Designs extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  void design1(BuildContext context) {
-    var alert = Dialog(
-      child: CustomContainer(height: 450, widget: Design1()),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
-  void design2(BuildContext context) {
-    var alert = Dialog(
-      child: CustomContainer(height: 240, widget: Design2()),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
-  void design3(BuildContext context) {
-    var alert = Dialog(
-      child: CustomContainer(height: 580, widget: Design3()),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
-  }
-
-  void design4(BuildContext context) {
-    var alert = Dialog(
-      child: CustomContainer(height: 500, widget: Design4()),
-    );
-
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return alert;
-        });
   }
 }
